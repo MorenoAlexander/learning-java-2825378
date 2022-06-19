5e42a3065bf44b8c303ff79a10c7bb697765b6fe
Alexander Moreno <am@alexandermoreno.com>
2022-06-19 03:22:55 -0500

    ysnc

--- com/main/Main.java
+++ com/main/Main.java
@@ -1,20 +1,18 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
-        double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
+        System.out.println("Pick a number between 1 and 100");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName + " " + studentLastName);
+        int inputtedNum = scanner.nextInt();
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy the good luck a friend brings you");
+        }
+        else {
+            System.out.println("Your shoe selection will make you very happy today");
+        }
     }
 }
